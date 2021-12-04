69e74662369836598d06c52a70012db92b8c335f
lexashvetsoff <75941967+lexashvetsoff@users.noreply.github.com>
2021-12-04 22:12:50 +0800

    Associated numbers with code

--- fetch_spacex.py
+++ fetch_spacex.py
@@ -3,6 +3,7 @@ import load_files as lf
 
 
 def fetch_spacex_last_launch(dirname):
+    FLIGHT_NUMBER = 93
 
     lf.path_check(dirname)
 
@@ -11,7 +12,7 @@ def fetch_spacex_last_launch(dirname):
     response = requests.get(url)
     response.raise_for_status()
 
-    answer = response.json()[93]
+    answer = response.json()[FLIGHT_NUMBER]
 
     for image_number, image_link in enumerate(answer['links']['flickr']['original']):
         lf.load_image(image_link, f'Space{image_number}.jpg', dirname)
