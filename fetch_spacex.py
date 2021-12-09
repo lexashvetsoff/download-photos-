e557e9e17f8cb9a326727ef30b66594a833d87bf
lexashvetsoff <75941967+lexashvetsoff@users.noreply.github.com>
2021-12-09 20:18:15 +0800

    Исправлен в названиях регистр букв

--- fetch_spacex.py
+++ fetch_spacex.py
@@ -3,14 +3,14 @@ import load_files as lf
 
 
 def fetch_spacex_last_launch(dirname):
-    FLIGHT_NUMBER = 93
+    flight_number = 93
 
     url = 'https://api.spacexdata.com/v4/launches'
 
     response = requests.get(url)
     response.raise_for_status()
 
-    answer = response.json()[FLIGHT_NUMBER]
+    answer = response.json()[flight_number]
 
     for image_number, image_link in enumerate(answer['links']['flickr']['original']):
         lf.load_image(image_link, f'Space{image_number}.jpg', dirname)
